4742087d33608d08026e2f7bb3d0c541158989e2
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-04-18 10:59:36 -0400

    typo

--- src/hisp/model.py
+++ src/hisp/model.py
@@ -337,7 +337,7 @@ class Model:
         elif pulse.pulse_type == "FP" or pulse.pulse_type == "FP_D":
             # rtol = 1e-10
             if relative_time % pulse.total_duration > pulse.duration_no_waiting:
-                rtol = 1e-111
+                rtol = 1e-11
             else:
                 rtol = 1e-5
         else:
