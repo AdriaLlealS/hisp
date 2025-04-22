f2c5d4312592af05613b6bc321796234dc66d8c0
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-04-22 11:37:59 -0400

    glow

--- src/hisp/model.py
+++ src/hisp/model.py
@@ -331,15 +331,15 @@ class Model:
         pulse = self.scenario.get_pulse(t)
         relative_time = t - self.scenario.get_time_start_current_pulse(t)
         if pulse.pulse_type == "GDC" or pulse.pulse_type == "ICWC":
-            rtol = 1e-11
+            rtol = 1e-10
         elif pulse.pulse_type == "BAKE":
             rtol = 1e-12
         elif pulse.pulse_type == "FP" or pulse.pulse_type == "FP_D":
             # rtol = 1e-10
             if relative_time % pulse.total_duration > pulse.duration_no_waiting:
-                rtol = 1e-11
+                rtol = 1e-12  # 1e-12
             else:
-                rtol = 1e-5
+                rtol = 1e-6  # 1e-6
         else:
             rtol = 1e-10
         return rtol
