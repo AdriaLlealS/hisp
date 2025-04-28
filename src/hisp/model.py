996e65d52ac870d9e20bff9341f247f4056ecec6
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-04-28 13:15:20 -0400

    bake

--- src/hisp/model.py
+++ src/hisp/model.py
@@ -333,7 +333,7 @@ class Model:
         if pulse.pulse_type == "GDC" or pulse.pulse_type == "ICWC":
             rtol = 1e-10
         elif pulse.pulse_type == "BAKE":
-            rtol = 1e-12
+            rtol = 1e-11
         elif pulse.pulse_type == "FP" or pulse.pulse_type == "FP_D":
             # rtol = 1e-10
             if relative_time % pulse.total_duration > pulse.duration_no_waiting:
