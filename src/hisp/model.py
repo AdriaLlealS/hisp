fd9f8a469b614f9ec018612fc8dae15a3b4e86cf
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-05-20 17:04:45 -0400

    tols

--- src/hisp/model.py
+++ src/hisp/model.py
@@ -339,7 +339,7 @@ class Model:
             if relative_time % pulse.total_duration > pulse.duration_no_waiting:
                 rtol = 1e-12  # 1e-12
             else:
-                rtol = 1e-10  # 1e-6 or 1e-8
+                rtol = 1e-11  # 1e-6 or 1e-8
         elif pulse.pulse_type == "RISP":
             rtol = 1e-10
         else:
