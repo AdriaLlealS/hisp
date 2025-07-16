d0b0ab4ef9b76de39b27fc9c75ee86f3b23c490d
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-07-16 15:57:27 -0300

    tols

--- src/hisp/model.py
+++ src/hisp/model.py
@@ -221,7 +221,7 @@ class Model:
                 else:
                     value = pulse.duration_no_waiting / 10
             elif pulse.pulse_type == "BAKE":
-                value = pulse.duration_no_waiting / 10000  # usually /10
+                value = pulse.duration_no_waiting / 1000  # usually /10
             elif pulse.pulse_type == "FP_D":
                 if relative_time_within_sub_pulse < pulse.duration_no_waiting:
                     value = 0.001  # s
@@ -341,7 +341,7 @@ class Model:
             elif pulse.pulse_type == "FP_D":
                 rtol = 1e-9
             else:
-                rtol = 1e-8  # 1e-6 or 1e-8 most recently
+                rtol = 1e-11  # 1e-6 or 1e-8 most recently
         elif pulse.pulse_type == "RISP":
             rtol = 1e-7
         else:
