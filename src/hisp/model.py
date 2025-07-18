d5e46e6c046101c0d5c12fbe57b238c87b5f0b2c
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-07-18 15:54:48 -0300

    tols

--- src/hisp/model.py
+++ src/hisp/model.py
@@ -217,7 +217,7 @@ class Model:
             # the stepsize is 1/10 of the duration of the pulse
             if pulse.pulse_type == "FP":
                 if relative_time_within_sub_pulse < pulse.duration_no_waiting:
-                    value = 0.01  # s
+                    value = 0.001  # s # usually 0.01
                 else:
                     value = pulse.duration_no_waiting / 10
             elif pulse.pulse_type == "BAKE":
