7bcf2914712e6cc2c5e18c0f3f440934416dc3fa
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-07-18 15:53:48 -0300

    tols

--- src/hisp/model.py
+++ src/hisp/model.py
@@ -221,14 +221,14 @@ class Model:
                 else:
                     value = pulse.duration_no_waiting / 10
             elif pulse.pulse_type == "BAKE":
-                value = pulse.duration_no_waiting / 100  # usually /10
+                value = pulse.duration_no_waiting / 10000  # usually /10
             elif pulse.pulse_type == "FP_D":
                 if relative_time_within_sub_pulse < pulse.duration_no_waiting:
                     value = 0.001  # s
                 else:
                     value = pulse.duration_no_waiting / 10
             else:
-                value = pulse.duration_no_waiting / 100  # usually /100
+                value = pulse.duration_no_waiting / 1000  # usually /100
         return periodic_step_function(
             relative_time,
             period_on=pulse.duration_no_waiting,
@@ -333,7 +333,7 @@ class Model:
         if pulse.pulse_type == "GDC" or pulse.pulse_type == "ICWC":
             rtol = 1e-13  # 1e-12 for just glow scenario
         elif pulse.pulse_type == "BAKE":
-            rtol = 1e-13  # 1e-15 for just glow scenario
+            rtol = 1e-12  # 1e-15 for just glow scenario
         elif pulse.pulse_type == "FP" or pulse.pulse_type == "FP_D":
             # rtol = 1e-10
             if relative_time % pulse.total_duration > pulse.duration_no_waiting:
@@ -341,7 +341,7 @@ class Model:
             elif pulse.pulse_type == "FP_D":
                 rtol = 1e-9
             else:
-                rtol = 1e-8  # 1e-6 or 1e-8 most recently
+                rtol = 1e-12  # 1e-6 or 1e-8 most recently
         elif pulse.pulse_type == "RISP":
             rtol = 1e-7
         else:
