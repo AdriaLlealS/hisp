0398faadcda99ad00e69100fe6bc534d8344d6bc
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-07-23 17:04:45 -0300

    tols

--- src/hisp/model.py
+++ src/hisp/model.py
@@ -228,7 +228,7 @@ class Model:
                 else:
                     value = pulse.duration_no_waiting / 10
             else:
-                value = pulse.duration_no_waiting / 100  # usually /100
+                value = pulse.duration_no_waiting / 1000  # usually /100
         return periodic_step_function(
             relative_time,
             period_on=pulse.duration_no_waiting,
