0d198cc28629498d208067f1630a474f178af5e2
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-08-14 18:02:59 -0700

    tols

--- src/hisp/model.py
+++ src/hisp/model.py
@@ -343,7 +343,7 @@ class Model:
             else:
                 rtol = 1e-8  # 1e-6 or 1e-8 most recently
         elif pulse.pulse_type == "RISP":
-            rtol = 1e-7
+            rtol = 1e-6
         else:
             rtol = 1e-10
         return rtol
