6a06d899117b7d4cb6955610266617c72966ed3c
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-08-20 12:35:34 -0700

    tols

--- src/hisp/model.py
+++ src/hisp/model.py
@@ -221,7 +221,7 @@ class Model:
                 else:
                     value = pulse.duration_no_waiting / 10
             elif pulse.pulse_type == "BAKE":
-                value = pulse.duration_no_waiting / 1000  # usually /10
+                value = pulse.duration_no_waiting / 10000  # usually /10
             elif pulse.pulse_type == "FP_D":
                 if relative_time_within_sub_pulse < pulse.duration_no_waiting:
                     value = 0.001  # s
@@ -331,7 +331,7 @@ class Model:
         pulse = self.scenario.get_pulse(t)
         relative_time = t - self.scenario.get_time_start_current_pulse(t)
         if pulse.pulse_type == "GDC" or pulse.pulse_type == "ICWC":
-            rtol = 1e-11  # 1e-12 for just glow scenario
+            rtol = 1e-12  # 1e-12 for just glow scenario
         elif pulse.pulse_type == "BAKE":
             rtol = 1e-12  # 1e-15 for just glow scenario
         elif pulse.pulse_type == "FP" or pulse.pulse_type == "FP_D":
