ac14bd636639b20deba100fe0f023970b13e2002
AdriaLlealS <adrialleal1433@gmail.com>
2025-11-26 17:25:30 +0100

    init

--- src/hisp/festim_models/__init__.py
+++ src/hisp/festim_models/__init__.py
@@ -2,6 +2,9 @@ from .mb_model import (
     make_W_mb_model,
     make_B_mb_model,
     make_DFW_mb_model,
+    make_W_mb_model_oldBC,
+    make_B_mb_model_oldBC,
+    make_DFW_mb_model_oldBC,
     make_temperature_function,
     make_particle_flux_function,
 )
