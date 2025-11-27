f1d0c28193affdc195b93c3ad3d4227b99fdd5fb
AdriaLlealS <adrialleal1433@gmail.com>
2025-11-27 11:05:37 +0100

    even finer mesh for W

--- src/hisp/festim_models/mb_model.py
+++ src/hisp/festim_models/mb_model.py
@@ -779,7 +779,7 @@ def make_W_mb_model_oldBC(
         if xs[-1] < L: xs.append(L)
         return np.array(xs)
     
-    vertices_graded = graded_vertices(L=L, h0=L/12e9, r=1.015)
+    vertices_graded = graded_vertices(L=L, h0=L/12e9, r=1.007)
     my_model.mesh = F.Mesh1D(vertices_graded)
 
     # W material parameters
