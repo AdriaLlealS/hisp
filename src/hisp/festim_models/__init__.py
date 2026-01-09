38b7ea88fce8351a3737387aeab89168addf1715
AdriaLlealS <adrialleal1433@gmail.com>
2026-01-09 16:57:16 +0100

    deleting old imports

--- src/hisp/festim_models/__init__.py
+++ src/hisp/festim_models/__init__.py
@@ -1,10 +1,4 @@
 from .new_mb_model import (
-    make_W_mb_model,
-    make_B_mb_model,
-    make_DFW_mb_model,
-    make_W_mb_model_oldBC,
-    make_B_mb_model_oldBC,
-    make_DFW_mb_model_oldBC,
     make_temperature_function,
     make_particle_flux_function,
     compute_flux_values,
