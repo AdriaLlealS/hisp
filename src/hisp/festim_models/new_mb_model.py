d5f822652d7ad71b1e4b8650c310968290ff072b
AdriaLlealS <adrialleal1433@gmail.com>
2026-03-13 14:10:38 +0100

    going back to DG elements for traps

--- src/hisp/festim_models/new_mb_model.py
+++ src/hisp/festim_models/new_mb_model.py
@@ -629,7 +629,7 @@ def make_dynamic_mb_model(
     my_model.settings.stepsize = Stepsize(initial_value=1e-3)
 
     # Use CG elements for traps (instead of FESTIM default DG)
-    my_model._element_for_traps = "CG"
+    #my_model._element_for_traps = "CG"
     print(f"[model] Trap element type: {my_model._element_for_traps}")
 
     return my_model, quantities
